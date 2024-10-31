c5d9155faf82b94b4b27e9c59281154d20cf6f56
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-01 00:31:53 +0500

    updated login page to show proper error messages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,19 +5,35 @@ import SubmitBtn from "../components/SubmitBtn";
 import { customFetch } from "../utils/customFetch";
 import CentreTemplate from "./CentreTemplate";
 import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 export const action = async ({ request }) => {
   console.log('login action');
   const formData = Object.fromEntries(await request.formData())
-  const { data } = await customFetch.post('/auth/login', formData)
-  const { user } = data.user
-  console.log(user);
-  toast.success(`Welcome ${user}! you have logged in Successfully!`)
-  return redirect('/')
+  try {
+    const { data } = await customFetch.post('/auth/login', formData)
+    const { user } = data.user
+    console.log(user);
+    toast.success(`Welcome ${user}! you have logged in Successfully!`)
+    return redirect('/')
+  } catch (error) {
+    const errorMessage = error?.response?.data || error?.response?.statusText || 'Something Went Wrong'
+    if (errorMessage === 'Please verify your email') {
+      toast.warn(`Please verify your email. Check your email ${formData.email}`)
+      return redirect('/email-verification')
+    }
+    toast.error(errorMessage)
+    return null
+  }
 }
 
 
 const Login = () => {
+
+  useEffect(() => {
+    window.history.replaceState(null, '', window.location.href)
+  }, [])
+
   return (
     <CentreTemplate>
       <div className="mx-auto flex w-full justify-center mb-5">
