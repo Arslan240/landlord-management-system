56f6a384655c58f9fd376fe6f69af74b87a5e9b8
Arslan240 <arslan.jamil1322@gmail.com>
2024-10-31 15:26:45 +0500

    Updated error handle middleware to send appropriate status code and response message

--- server/middlewares/error-handler.middleware.js
+++ server/middlewares/error-handler.middleware.js
@@ -2,7 +2,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   const { statusCode, message } = err
   console.log(err.statusCode, err.message)
   console.log(err)
-  res.send(err.message)
+  res.status(statusCode || 500).send(message)
 }
 
 module.exports = errorHandlerMiddleware
