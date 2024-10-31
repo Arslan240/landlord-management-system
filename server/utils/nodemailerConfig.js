9df29a9d5985b20d570c19357a00e34fbb6c0d70
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-01 00:31:03 +0500

    updated config details

--- server/utils/nodemailerConfig.js
+++ server/utils/nodemailerConfig.js
@@ -1,10 +1,10 @@
 const nodemailerConfig = {
-  host: "smtp.ethereal.email",
+  host: 'smtp.ethereal.email',
   port: 587,
   auth: {
-    user: "zelda.considine47@ethereal.email",
-    pass: "xw6KyS2n1RGD2xjGSk",
-  },
+    user: 'arnaldo45@ethereal.email',
+    pass: 'ubwrpUHTbM4eZwccnW'
+  }
 }
 module.exports = {
   nodemailerConfig,
