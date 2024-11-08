b6fe652e895b567be9e293dbf759a783a9118708
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-09 02:12:55 +0500

    updated to add 404 error message

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -6,9 +6,11 @@ const Error = () => {
   const error = useRouteError();
   console.log(error); // This will help you see the structure of `error` in the console
 
+  const errorMessage = error.status === 404 ? '404. Oops, Page Not Found' : 'Oops, there was an error'
+
   return (
     <CentreTemplate>
-      <h1 className="text-5xl font-bold items-center text-neutral-800">Oops, there was an error</h1>
+      <h1 className="text-5xl font-bold items-center text-neutral-800">{errorMessage}</h1>
       <p>{error?.statusText || error?.message || "Something went wrong."}</p>
     </CentreTemplate>
   );
