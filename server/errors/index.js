000f802fb7f23f08f86522b94229a9afbb548c67
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-09 23:17:23 +0500

    updated index.js to include unauthorized error class

--- server/errors/index.js
+++ server/errors/index.js
@@ -1,7 +1,9 @@
 const { BadRequestError } = require("./BadRequestError")
 const { UnAuthenticateError } = require("./UnAuthenticatedError")
+const { UnAuthorizedError } = require("./UnAuthorizedError")
 
 module.exports = {
   UnAuthenticateError,
   BadRequestError,
+  UnAuthorizedError
 }
