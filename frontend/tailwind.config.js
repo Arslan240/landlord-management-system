a8d1504378ac3e2ac3f92ae2f4ed15e597a8fe57
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-09 14:45:45 +0500

    updated some color variables

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,18 +3,20 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    './node_modules/preline/preline.js',
+    // './node_modules/preline/preline.js',
   ],
   theme: {
     extend: {
       borderRadius: {
-        card: '1.25rem' 
+        card: '1.25rem'
       },
       colors: {
         primary: {
           DEFAULT: '#545C75',
           light: '#999BA6',
-          lightest: '#EBEFF3'
+          lightest: '#EBEFF3',
+          hover: '#d9d9dc',
+          backg: '#f9fafb'
         },
         secondary: {
           DEFAULT: '#6E62E5',
@@ -34,15 +36,15 @@ export default {
   },
   plugins: [
     // require('preline/plugin'),
-    // require("@tailwindcss/typography"),
+    require("@tailwindcss/typography"),
     require('daisyui')
   ],
   daisyui: {
     themes: [
       {
         mytheme: {
-          'primary' : '#545C75',
-          'secondary' : '#6E62E5',
+          'primary': '#545C75',
+          'secondary': '#6E62E5',
           'accent': '#23C35E',
           'warning': '#FD6365'
         }
