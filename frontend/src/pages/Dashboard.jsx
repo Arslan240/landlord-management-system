f435c998ba476bac09455391e9f9805445446f86
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-11 23:47:35 +0500

    added a loading component based on route loading, also added a ref on main section for further use in responsiveness based on width event listener that we'll put in future

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigation } from "react-router-dom"
 import Header from "../components/Header"
 import RequireAuth from "../components/RequireAuth"
 import Sidebar from "../components/Sidebar"
 import SidebarRenderer from "../components/SidebarRenderer"
 import Breadcrumbs from "../components/Breadcrumbs"
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useRef, useState } from "react"
+import Loading from "../components/Loading"
 
 const SidebarContext = createContext()
 
@@ -12,21 +13,33 @@ export const useSidebarContext = () => useContext(SidebarContext)
 
 const Dashboard = ({ children }) => {
   const [expanded, setExpanded] = useState(true)
+  const navigation = useNavigation()
+  const isLoading = navigation.state === 'loading'
+  
+  const outletRef = useRef()
 
   return (
     <>
-      <div className="flex h-screen">
+      <div className="flex h-screen ">
         {/* <Sidebar /> */}
         <SidebarContext.Provider value={{ expanded, setExpanded }}>
           <SidebarRenderer />
         </SidebarContext.Provider>
-        <div className="flex flex-col w-full bg-primary-backg">
+        <div className="flex flex-col w-full bg-primary-backg ">
           <Header />
           <Breadcrumbs />
-          <main className={`flex-grow padding dashboard-outlet transition-all delay-[50] ${expanded ? 'w-[100%]' : 'w-[85%]'} mx-auto `}>
-            Dashboard
-            <Outlet />
-          </main>
+          {
+            isLoading ? <Loading /> : (
+              <main className={`flex-grow padding dashboard-outlet transition-all delay-[50] text-sm w-full 
+                ${expanded && 'w-full'} 
+                sm:w-full md:${expanded ? 'w-full' : 'w-[85%]'} lg:${expanded ? 'w-full' : 'w-[85%]'} xl:${expanded ? 'w-full' : 'w-[85%]'} 3xl:w-[75%] mx-auto overflow-y-auto`
+              }
+                ref={outletRef}
+              >
+                <Outlet />
+              </main>
+            )
+          }
         </div>
       </div>
     </>
