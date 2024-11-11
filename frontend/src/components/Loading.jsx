eb8040c51ca288830428fc1faefbe9ac20f42a79
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-11 23:34:56 +0500

    Currently not for Route Loading Component. its a simple loading component to use in different components when fetching data.

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,7 @@
+const Loading = () => {
+  return (
+    <div className="py-20 flex justify-center"><span className="loading loading-spinner loading-md" ></span></div>
+  )
+}
+
+export default Loading
