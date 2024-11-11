dfd3ed5f396b6d8c85571ffd9dd69773a03fe945
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-12 03:12:36 +0500

    some style changes in outlet section. Idk why but the conditional styles stopped working properly. Now, this is also acceptable but not great

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -15,7 +15,6 @@ const Dashboard = ({ children }) => {
   const [expanded, setExpanded] = useState(true)
   const navigation = useNavigation()
   const isLoading = navigation.state === 'loading'
-  
   const outletRef = useRef()
 
   return (
@@ -30,11 +29,10 @@ const Dashboard = ({ children }) => {
           <Breadcrumbs />
           {
             isLoading ? <Loading /> : (
-              <main className={`flex-grow padding dashboard-outlet transition-all delay-[50] text-sm w-full 
+              <main className={`flex-grow padding dashboard-outlet transition-all delay-[50] text-sm  
                 ${expanded && 'w-full'} 
-                sm:w-full md:${expanded ? 'w-full' : 'w-[85%]'} lg:${expanded ? 'w-full' : 'w-[85%]'} xl:${expanded ? 'w-full' : 'w-[85%]'} 3xl:w-[75%] mx-auto overflow-y-auto`
+                md:${expanded ? 'w-full' : 'w-[85%]'} lg:${expanded ? 'w-full' : 'w-[85%]'} xl:${expanded ? 'w-full' : 'w-[85%]'} 3xl:w-[75%] mx-auto overflow-y-auto `
               }
-                ref={outletRef}
               >
                 <Outlet />
               </main>
