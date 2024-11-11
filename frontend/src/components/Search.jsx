f5c4e01f710c1f9e69f9f8d1e041c3dfe194b5d7
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-11 23:36:27 +0500

    added classes to change background color when not in header, add a changeHandler to enhance reusability

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,16 +1,17 @@
 import { SearchIcon } from "lucide-react"
 
-const Search = ({ classNames, placeholder }) => {
+const Search = ({ classNames, placeholder, header = false, changeHandler }) => {
   return (
     <div className={` space-y-3 ${classNames}`}>
       <div className="relative">
         <input
           type="text"
-          className="peer py-3 px-4 ps-11 block w-full  border-transparent rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+          className={`peer py-3 px-4 ps-11 block w-full  border-transparent ${!header && 'bg-primary-lightest'} rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none`}
           placeholder={placeholder}
+          onChange={changeHandler}
         />
         <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-3 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
-          <SearchIcon size={'1.3rem'} />
+          <SearchIcon size={`${!header ? '1.1rem' : '1.3rem'}`} />
         </div>
       </div>
     </div>
