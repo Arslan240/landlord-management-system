a675094c2c0890303f2078a36e5507de408839b5
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-12 18:07:17 +0500

    just added ref and commented out something

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,6 +1,6 @@
 import { SearchIcon } from "lucide-react"
 
-const Search = ({ classNames, placeholder, header = false, changeHandler }) => {
+const Search = ({ classNames, placeholder, header = false, changeHandler, ref }) => {
   return (
     <div className={` space-y-3 ${classNames}`}>
       <div className="relative">
@@ -8,7 +8,8 @@ const Search = ({ classNames, placeholder, header = false, changeHandler }) => {
           type="text"
           className={`peer py-3 px-4 ps-11 block w-full  border-transparent ${!header && 'bg-primary-lightest'} rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none`}
           placeholder={placeholder}
-          onChange={changeHandler}
+          // onChange={changeHandler}
+          ref={ref}
         />
         <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-3 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
           <SearchIcon size={`${!header ? '1.1rem' : '1.3rem'}`} />
