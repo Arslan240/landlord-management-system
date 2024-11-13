fcd096fed1a2cb8d2c93c760f53b8630ea8e9201
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-13 17:16:02 +0500

    added a new reducer for setServerFiltersAndProperties also updated setFilters

--- frontend/src/redux/propertySlice.js
+++ frontend/src/redux/propertySlice.js
@@ -17,31 +17,48 @@ const propertySlice = createSlice({
     // updated setFilters
     setFilters: (state, action) => {
       const { name, value } = action.payload
-      if (!name || !value) {
+
+      if (!name) {
         toast.error("Provide both name and value in redux")
         return
       }
-      state.filters = { ...state.filters, [name]: { value } }
+
+      state.filters = {
+        ...state.filters,
+        [name]: {
+          ...state.filters[name],
+          value,
+        },
+      }
+      console.log(action.payload)
     },
     // figure out a way, how to maintain selected value state when a new request is made. maybe before storing filters, check if selected value is present then use that. And based on that update styles properly.
     setServerFilters: (state, action) => {
+      console.log("state before updated")
+
       let serverFilters = action.payload
       let newFilters = {}
 
       Object.keys(serverFilters).forEach((key) => {
         const singleFilter = serverFilters[key]
-        newFilters[singleFilter.name] = {
-          selectedValue: "",
+        const { name } = singleFilter
+        const prevSelect = state.filters[name]?.selectedValue || 10
+        newFilters[name] = {
+          selectedValue: prevSelect === "" ? "" : prevSelect,
           ...singleFilter,
         }
       })
-      console.log(serverFilters)
+      console.log(newFilters)
       state.filters = newFilters
+    },
+    setServerFiltersAndProperties: (state, action) => {
+      state.properties = action.payload.properties
+      state.filters = action.payload.filters
     },
   },
 })
 
-export const { setProperties, setFilters, setServerFilters } = propertySlice.actions
+export const { setProperties, setFilters, setServerFilters, setServerFiltersAndProperties } = propertySlice.actions
 
 export const usePropertyState = () => useSelector((state) => state.propertyState)
 
