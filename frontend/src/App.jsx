f6b4fc3058e62a2a1255a5226d85349f30097ea1
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-13 17:16:48 +0500

    added react query dev tools

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,85 +1,86 @@
 // src/App.jsx
-import 'preline'; // Import to ensure JS behaviors work
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import "preline" // Import to ensure JS behaviors work
+import React from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
-import HomeLayout from './components/HomeLayout';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Error from './pages/Error';
-import EmailVerification from './pages/EmailVerification';
-import VerifyEmail from './pages/VerifyEmail';
+import HomeLayout from "./components/HomeLayout"
+import Landing from "./pages/Landing"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Error from "./pages/Error"
+import EmailVerification from "./pages/EmailVerification"
+import VerifyEmail from "./pages/VerifyEmail"
 
 // actions
-import { action as registerAction } from './pages/Register'
-import { action as loginAction } from './pages/Login'
-import { action as verifyAction } from './pages/VerifyEmail'
-import { store } from './redux/store';
-import Dashboard from './pages/Dashboard';
-import Properties from './pages/Properties';
+import { action as registerAction } from "./pages/Register"
+import { action as loginAction } from "./pages/Login"
+import { action as verifyAction } from "./pages/VerifyEmail"
+import { store } from "./redux/store"
+import Dashboard from "./pages/Dashboard"
+import Properties from "./pages/Properties"
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: {}
-  }
+    queries: {},
+  },
 })
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HomeLayout />,
     errorElement: <Error />,
     children: [
       {
         index: true,
-        element: <Landing />
+        element: <Landing />,
       },
       {
-        path: 'dashboard',
+        path: "dashboard",
         element: <Dashboard />,
         children: [
           {
-            path: 'properties',
-            element: <Properties />
-          }
-        ]
-      }
-    ]
+            path: "properties",
+            element: <Properties />,
+          },
+        ],
+      },
+    ],
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
     action: loginAction,
-    errorElement: <Error />
+    errorElement: <Error />,
   },
   {
-    path: '/register',
+    path: "/register",
     element: <Register />,
     action: registerAction,
     errorElement: <Error />,
   },
   {
-    path: '/email-verification',
+    path: "/email-verification",
     element: <EmailVerification />,
-    errorElement: <Error />
+    errorElement: <Error />,
   },
   {
-    path: '/verify-email',
+    path: "/verify-email",
     element: <VerifyEmail />,
     action: verifyAction,
-    errorElement: <Error />
-  }
-
+    errorElement: <Error />,
+  },
 ])
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools />
     </QueryClientProvider>
-  );
+  )
 }
 
-export default App;
+export default App
