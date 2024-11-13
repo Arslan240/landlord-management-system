767e0cf08c77cefd76dfc8402361884ce2ee9866
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-13 17:09:59 +0500

    added filters rendering from redux filters and a new function for returning appropriate icon for each

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react"
 import Dropdown from "./Dropdown"
+import { usePropertyState } from "../redux/propertySlice"
+import { Bath, Bed, CarFront } from "lucide-react"
 
 // find a way to render filters from array values.
 // preferrabely server will send us filter values which we'll use to render filters. (server will know which unique values are present, so we'll only render those options which are actually present in the backend)
 const Filters = () => {
   // fetch values of filter values
+  const { filters } = usePropertyState()
+
+  const sortedFilters = Object.values(filters).sort((a, b) => a.index - b.index)
+
   return (
-    <div className="py-3 md:py-0">
-      <SingleFilter name={"price"} icon={"$"} />
-      <SingleFilter name={"sqft"} icon={"m2"} />
+    <div className="py-3 md:py-0 flex flex-wrap text-primary">
+      {sortedFilters.map((filter) => {
+        let { name } = filter
+
+        return <SingleFilter name={name} icon={getIcon(name)} />
+      })}
+      {/* <SingleFilter name={"price"} icon={"$"} />
+      <SingleFilter name={"sqft"} icon={"m2"} /> */}
       {/* <SingleFilter name={'beds'} />
       <SingleFilter name={'baths'} /> */}
     </div>
@@ -18,10 +29,11 @@ export default Filters
 
 const SingleFilter = ({ name, icon }) => {
   // if a value is selected then change border color to primary ligh and background to lightest to show that this filter is being used.
+  // const { filters } = usePropertyState()
 
   return (
     <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn rounded-full hover:bg-primary-hover m-1 h-9 min-h-9">
+      <div tabIndex={0} role="button" className="btn rounded-full hover:bg-primary-hover m-1 h-7 min-h-7 font-medium">
         {icon && <span>{icon}</span>}
         <span>{name}</span>
       </div>
@@ -31,18 +43,19 @@ const SingleFilter = ({ name, icon }) => {
 }
 
 const getIcon = (name) => {
+  const iconSize = "1rem"
   switch (name) {
     case "rent": {
       return "$"
     }
-    case "sqft": {
-      return "sqft"
-    }
     case "beds": {
-      return ""
+      return <Bed size={iconSize} />
     }
     case "baths": {
-      return ""
+      return <Bath size={iconSize} />
+    }
+    case "garages": {
+      return <CarFront size={iconSize} />
     }
     default: {
       return ""
