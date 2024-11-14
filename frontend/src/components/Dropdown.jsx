1553698c765e7ffd48efbf55fbd44c60c3545809
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-15 01:34:39 +0500

    Dropdown has become a reusable component now mostly. I updated dropdown to render slider for values, input text also. I've added comments to explain what things are doing

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -1,26 +1,128 @@
+import "rc-slider/assets/index.css"
+import "rc-tooltip/assets/bootstrap.css"
+
 import { useDispatch } from "react-redux"
 import { setFilters } from "../redux/propertySlice"
+import { useEffect, useRef, useState } from "react"
+import TooltipSlider from "./ToolTipSlider"
+import FormInput from "./FormInput"
+import { debounce, rest } from "lodash"
+import { toast } from "react-toastify"
 
-// make it reusable by passing down setFilters from parent so that we can use it with slice. if we keep the same syntax with setFilters function, we can reuse it.
-const Dropdown = ({ items = [], name }) => {
+// HOW IT WORKS
+// we store slider values in sliderValues, and input value in entryRef.
+// Then by using onChangeComplete we update manualEntry and selectedSliderValues states.
+// Because based on these states we dispatch to redux. When both of these states are updated we also update type to keep
+// track of which one was latest and only send that one to setFilters.
+
+// First, we recieve min and max from Filters, we update the default state using useEffect.
+// using min and max we render slider.
+// sliderValues state is for just UI, onChange updates this state on every single up or down in number
+// we pass these to values props which are used by tooltip to show current value for min and max on top of handle.
+// we use ref for input, so that we only update redux only once using onSubmit.
+// TODO: maybe in future, synchronize input and sliders, when a number entered in input make slider same to it as well. and vice versa.
+
+// added type to see which state was updated latest, so only that value for this filter will be passed to redux store.
+const MANUAL = "MANUAL"
+const SLIDER = "SLIDER"
+
+const Dropdown = ({ name, min = 0, max = 700 }) => {
   const dispatch = useDispatch()
-
-  const handleFilter = (name, value) => {
-    dispatch(setFilters({ name, value }))
+  const [type, setType] = useState(null)
+  // text input
+  const [manualEntry, setManualEntry] = useState()
+  const entryRef = useRef(null)
+  // slider state
+  const [sliderValues, setSliderValues] = useState([min, max])
+  const [selectedSliderValues, setSelectedSliderValues] = useState()
+  // these are for start and end labels for slider, before I was using values from sliderValues state, which caused them to change with the values
+  const marks = {
+    [min]: [min],
+    [max]: [max],
   }
+
+  // initial values for slider.
+  useEffect(() => {
+    setSliderValues([min, max])
+  }, [min, max])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    let { value } = entryRef.current
+    if (isNaN(value)) {
+      toast.error("Please enter numbers only")
+      return
+    }
+    setManualEntry(Number(value))
+    setType(MANUAL)
+  }
+
+  // to properly updated tooltips
+  const handleSliderChange = (sliderValues) => {
+    setSliderValues(sliderValues)
+  }
+
+  // to update state only when user stops sliding, we'll use this to send upto redux for react query to refetch properties.
+  const handleChangeComplete = (values) => {
+    setSelectedSliderValues(values)
+    setType(SLIDER)
+  }
+
+  console.log(manualEntry, selectedSliderValues)
+
+  // debouncedFilter function
+  const debouncedFilter = debounce((name, value) => {
+    console.log("debounced Filter", { name, ...value })
+    dispatch(setFilters({ name, ...value }))
+  }, 500)
+
+  // we update redux state based on which type was set latest by user.
+  useEffect(() => {
+    if (!type) return //for initial render
+    console.log(type)
+    if (type === MANUAL) {
+      debouncedFilter(name, { min: manualEntry })
+    } else if (type === SLIDER) {
+      debouncedFilter(name, { min: sliderValues[0], max: sliderValues[1] })
+    }
+  }, [type, manualEntry, sliderValues])
+
   return (
-    <ul
-      tabIndex={0}
-      className=' menu dropdown-content rounded-md z-[1] w-52 p-1.5 shadow bg-primary-lightest'
-    >
-      {items.map((item, index) => (
-        <li className='capitalize hover:bg-secondary-lightest rounded-md '>
-          <a onClick={() => handleFilter(name, item)} key={`${item}${index}`}>
-            {item}
-          </a>
-        </li>
-      ))}
+    <ul tabIndex={0} id="dropdown" className=" menu dropdown-content rounded-md z-[1] w-52 p-1.5 shadow bg-primary-lightest">
+      <div></div>
+      <form className="px-1" onSubmit={submitHandler}>
+        <FormInput dropdown placeholder={"Enter value"} ref={entryRef} />
+      </form>
+      <div className="px-5 pb-7">
+        <TooltipSlider
+          value={sliderValues}
+          range
+          min={min}
+          max={max}
+          marks={marks}
+          defaultValue={sliderValues}
+          tipFormatter={(value) => `${value}`}
+          onChange={handleSliderChange}
+          onChangeComplete={handleChangeComplete}
+          styles={{ track: { backgroundColor: "#7566ff" }, handle: { borderColor: "#7566ff", boxShadow: "#7566ff" } }}
+        />
+      </div>
     </ul>
   )
 }
 export default Dropdown
+
+// simple implementation
+{
+  /* <Slider
+         value={sliderValues}
+          range
+          min={min}
+          max={max}
+          marks={marks}
+          defaultValue={sliderValues}
+          onChange={handleSliderChange}
+          onChangeComplete={handleChangeComplete}
+          styles={{ track: { backgroundColor: "#7566ff" }, handle: { borderColor: "#7566ff", boxShadow: "#7566ff" } }}
+        /> */
+}
