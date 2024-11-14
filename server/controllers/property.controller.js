2ec48e23dae68132b233e9a72fbea92ffe1b28c4
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-15 03:57:49 +0500

    changed default limit to 10 and spelling correction of bathrooms to baths and bedrooms to beds in getAllProperties controller

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -76,7 +76,7 @@ const getAllProperties = async (req, res) => {
   const propertyFilters = await generatePropertiesFilters()
   const totalItems = await Property.find().countDocuments({ ...query })
 
-  const limit = Number(req.query.limit) || 2
+  const limit = Number(req.query.limit) || 10
   const page = Number(req.query.page) || 1
   const skip = (page - 1) * limit
   const pageCount = Math.ceil(totalItems / limit)
@@ -137,10 +137,10 @@ const addProperty = async (req, res) => {
   if (!details.sqft) {
     throw new BadRequestError("Please provide sqft")
   }
-  if (!details.bathrooms) {
+  if (!details.baths) {
     throw new BadRequestError("Please provide number of bathrooms")
   }
-  if (!details.bedrooms) {
+  if (!details.beds) {
     throw new BadRequestError("Please provide number of bedrooms")
   }
   if (!address) {
