7d3db05a8f3bc274a295e35e7524a86bf7d62dd7
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-15 01:43:47 +0500

    removed unused ref

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,22 +1,22 @@
 import { SearchIcon } from "lucide-react"
 
-const Search = ({ classNames, placeholder, header = false, changeHandler, ref }) => {
+const Search = ({ classNames, placeholder, header = false, changeHandler }) => {
   return (
     <div className={` space-y-3 ${classNames}`}>
       <div className="relative">
         <input
           type="text"
-          className={`peer py-3 px-4 ps-11 block w-full  border-transparent ${!header && 'bg-primary-lightest'} rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none`}
+          className={`peer py-3 px-4 ps-11 block w-full  border-transparent ${
+            !header && "bg-primary-lightest"
+          } rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none`}
           placeholder={placeholder}
           // onChange={changeHandler}
-          ref={ref}
         />
         <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-3 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
-          <SearchIcon size={`${!header ? '1.1rem' : '1.3rem'}`} />
+          <SearchIcon size={`${!header ? "1.1rem" : "1.3rem"}`} />
         </div>
       </div>
     </div>
-
   )
 }
 export default Search
