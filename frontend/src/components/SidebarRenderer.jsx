a4ffa10e8665aa8c765fb6cb4c7c9e768b408df3
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-15 01:44:48 +0500

    added key to SidebarItem

--- frontend/src/components/SidebarRenderer.jsx
+++ frontend/src/components/SidebarRenderer.jsx
@@ -2,32 +2,32 @@ import { Building, LayoutGrid } from "lucide-react"
 import { useLocation } from "react-router-dom"
 import Sidebar, { SidebarItem } from "./Sidebar"
 
-const iconSize = '1.3rem'
+const iconSize = "1.3rem"
 
 const SidebarRenderer = () => {
   const location = useLocation()
   const sidebarRoutes = [
     {
       icon: <LayoutGrid size={iconSize} />,
-      text: 'Dashboard',
-      to: '/dashboard',
-      active: location.pathname === '/dashboard' || location.pathname === '/dashboard/',
-      alert: false
+      text: "Dashboard",
+      to: "/dashboard",
+      active: location.pathname === "/dashboard" || location.pathname === "/dashboard/",
+      alert: false,
     },
     {
-      icon: <Building size={iconSize}/>,
-      text: 'Properties',
-      to: '/dashboard/properties',
-      active: location.pathname === '/dashboard/properties',
-      alert: false
-    }
+      icon: <Building size={iconSize} />,
+      text: "Properties",
+      to: "/dashboard/properties",
+      active: location.pathname === "/dashboard/properties",
+      alert: false,
+    },
   ]
 
   return (
     <>
       <Sidebar>
-        {sidebarRoutes.map(item => {
-          return <SidebarItem {...item} />
+        {sidebarRoutes.map((item, index) => {
+          return <SidebarItem {...item} key={index} />
         })}
       </Sidebar>
     </>
