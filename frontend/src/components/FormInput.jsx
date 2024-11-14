bd06dd7593e0086e05a536d901138b46997f8164
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-15 00:49:16 +0500

    added forwardRef to it so input can be assigned ref if parent component decides to. It was needed in Dropdown component.

--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -1,16 +1,26 @@
-const FormInput = ({ label, type, placeholder, disabled, rightLabel, ...rest }) => {
+import { forwardRef } from "react"
+
+const FormInput = forwardRef(({ label, type, placeholder, disabled, rightLabel, dropdown, ...rest }, ref) => {
+  const padding = dropdown ? `p-2` : `p-4`
   return (
     <>
       <div className="w-full my-3">
         <div className="flex justify-between items-center ">
-          <label htmlFor="" className="block text-sm font-medium mb-2 capitalize">{label}</label>
+          <label htmlFor="" className="block text-sm font-medium mb-2 capitalize">
+            {label}
+          </label>
           <span className="block mb-2 text-sm text-gray-500 ">{rightLabel}</span>
         </div>
-        <input type={type} id="" className="p-4 block w-full border-gray-200 rounded-lg text-sm focus:border-secondary focus:ring-border-secondary disabled:opacity-50 disabled:pointer-events-none"
-          disabled={disabled} placeholder={placeholder} {...rest}/>
+        <input
+          type={type}
+          ref={ref}
+          className={`${padding} block w-full border-gray-200 rounded-lg text-sm focus:border-secondary focus:ring-border-secondary disabled:opacity-50 disabled:pointer-events-none`}
+          disabled={disabled}
+          placeholder={placeholder}
+          {...rest}
+        />
       </div>
     </>
-
   )
-}
+})
 export default FormInput
