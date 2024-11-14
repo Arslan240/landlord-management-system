11c06181915c7c1ecc5e3aae92d8686f05275aaf
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-15 01:42:08 +0500

    just some schenanigans by prettier

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,48 +8,49 @@ export default {
   theme: {
     extend: {
       borderRadius: {
-        card: '1.25rem'
+        card: "1.25rem",
       },
       colors: {
         primary: {
-          DEFAULT: '#545C75',
-          light: '#999BA6',
-          lightest: '#EBEFF3',
-          hover: '#d9d9dc',
-          backg: '#f9fafb'
+          DEFAULT: "#545C75",
+          light: "#999BA6",
+          lightest: "#EBEFF3",
+          hover: "#d9d9dc",
+          backg: "#f9fafb",
         },
         secondary: {
-          DEFAULT: '#6E62E5',
-          light: '#bfb8ff',
-          lightest: '#e1defe'
+          DEFAULT: "#6E62E5",
+          light: "#bfb8ff",
+          lightest: "#e1defe",
         },
         accent: {
-          DEFAULT: '#23C35E',
-          light: '#b9f6ce'
+          DEFAULT: "#23C35E",
+          light: "#b9f6ce",
         },
         warning: {
-          DEFAULT: '#FD6365',
-          light: '#FFE3E3'
+          DEFAULT: "#FD6365",
+          light: "#FFE3E3",
         },
-      }
+      },
     },
   },
   plugins: [
     // require('preline/plugin'),
     require("@tailwindcss/typography"),
-    require('daisyui')
+    require("daisyui"),
   ],
   daisyui: {
     themes: [
       {
         mytheme: {
-          'primary': '#545C75',
-          'secondary': '#6E62E5',
-          'accent': '#23C35E',
-          'warning': '#FD6365'
-        }
+          primary: "#545C75",
+          secondary: "#6E62E5",
+          accent: "#23C35E",
+          warning: "#FD6365",
+        },
       },
-      'winter', 'dracula']
-  }
+      "winter",
+      "dracula",
+    ],
+  },
 }
-
