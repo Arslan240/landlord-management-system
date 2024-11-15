314564dadc8164ae5b66666f5ec84c33db900748
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-15 21:39:35 +0500

    update filters UI to show they have been selected using selected and setSelected. conditional rendering of classes based on selected. Also pass setSelected to Dropdown to use when values are reset. Inside Filters component created handleReset and assigned to reset button, to set isReset to false, which is then used by SingleFilters and Dropdown to reset their state

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -2,20 +2,33 @@ import React, { createContext, useContext, useState } from "react"
 import Dropdown from "./Dropdown"
 import { usePropertyState } from "../redux/propertySlice"
 import { Bath, Bed, CarFront, Filter } from "lucide-react"
+import { useDispatch } from "react-redux"
 
 const FiltersContext = createContext()
 
-const Filters = React.memo(({ serverFilters = [] }) => {
+const Filters = React.memo(({ serverFilters = [], resetFilters }) => {
+  const dispatch = useDispatch()
   const sortedFilters = Object.values(serverFilters).sort((a, b) => a.index - b.index)
+  const [isReset, setIsReset] = useState(false)
   console.log("Filters rendered", sortedFilters)
 
+  const handleResetFilters = () => {
+    if (isReset) return
+    dispatch(resetFilters())
+    setIsReset(true)
+    console.log("reset filters")
+  }
+
   return (
-    <FiltersContext.Provider value={serverFilters}>
+    <FiltersContext.Provider value={{ serverFilters, isReset, setIsReset }}>
       <div className="py-3 md:py-0 flex flex-wrap text-primary items-center gap-[0.1rem] ">
         {sortedFilters.map((filter, index) => {
           let { name } = filter
           return <SingleFilter name={name} key={`${name}${index}`} />
         })}
+        <button className="btn btn-warning h-8 min-h-8 text-white text-sm" onClick={handleResetFilters}>
+          Reset
+        </button>
       </div>
     </FiltersContext.Provider>
   )
@@ -25,19 +38,27 @@ export default Filters
 Filters.whyDidYouRender = true
 
 const SingleFilter = ({ name }) => {
-  const serverFilters = useFiltersContext()
+  const { serverFilters, isReset, setIsReset } = useFiltersContext()
+  const [selected, setSelected] = useState(false)
   const { min, max } = serverFilters[name]
 
   const icon = getIcon(name)
   // if a value is selected then change border color to primary ligh and background to lightest to show that this filter is being used.
 
   return (
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn rounded-full hover:bg-primary-hover m-1 h-7 min-h-7 font-medium">
+    <div className={`dropdown dropdown-end `}>
+      <div
+        tabIndex={0}
+        role="button"
+        className={`btn rounded-full hover:bg-primary-hover hover:text-secondary-bright m-1 h-7 min-h-7 font-medium ${
+          selected && "border-secondary-bright text-secondary-bright bg-secondary-light"
+        }`}
+      >
         {icon && <span>{icon}</span>}
         <span>{name}</span>
+        <span className={`${!selected && "hidden"} badge p-1 h-1 bg-indigo-800 absolute top-0 right-2`}></span>
       </div>
-      <Dropdown name={name} min={min} max={max} />
+      <Dropdown name={name} min={min} max={max} selected={selected} setSelected={setSelected} isReset={isReset} setIsReset={setIsReset} />
     </div>
   )
 }
