7b6555bd78e24bafea36249f411ddde6e0d4445d
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-16 22:44:52 +0500

    added to render PropertyRenderer, for list view and grid view option in properties page. Also added isSmall state based on which we render + or Add property text.

--- frontend/src/pages/Properties.jsx
+++ frontend/src/pages/Properties.jsx
@@ -7,7 +7,12 @@ import RequireAuth from "../components/RequireAuth"
 import { useDispatch, useSelector } from "react-redux"
 import { setServerFilters, usePropertyState, resetFilters } from "../redux/propertySlice"
 import Loading from "../components/Loading"
-import Property from "../components/Property"
+import Property from "../components/PropertyRenderer"
+import { times } from "lodash"
+import { LayoutGrid, List, Plus } from "lucide-react"
+import { Link, useLocation, useOutletContext } from "react-router-dom"
+import { GRIDVIEW, LISTVIEW } from "../constants"
+import PropertyRenderer from "../components/PropertyRenderer"
 
 // update these comments to explain actual functionality of how states and filters are being managed.
 // first we'll have only serverFilters and selectedFilters in propertySlice.
@@ -26,9 +31,18 @@ import Property from "../components/Property"
 // if value is selected using slider, we override min and max both with new values.
 // now our selectedFilters have lets say adequate values. Now we got to params object creation from filters.
 
+const iconSize = "1.3rem"
+const strokeWidth = "1.75"
+
 const Properties = () => {
+  const location = useLocation()
+  const [isSmall, setIsSmall] = useState(false)
+  const [pagination, setPagination] = useState(null)
   const [searchTerm, setSearchTerm] = useState("")
   const searchRef = useRef()
+
+  //
+  const [view, setView] = useState(LISTVIEW)
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value)
@@ -43,19 +57,34 @@ const Properties = () => {
     queryFn: async () => {
       let urlParams = createUrlParams(selectedFilters)
       const { data } = await customFetch(`properties?${urlParams}`)
-      return data.data
+      return data
     },
   })
 
-  const properties = data?.properties || []
+  const properties = data?.data?.properties || []
 
   // updating properties in redux
   useEffect(() => {
-    const serverFilters = data?.filters
+    const serverFilters = data?.data?.filters
+    const pagination = data?.meta?.pagination
     if (serverFilters) {
       dispatch(setServerFilters(serverFilters))
     }
+    if (pagination) {
+      setPagination(pagination)
+    }
   }, [data, dispatch])
+
+  // useEffect to track if it's greater than 640 or not similar to sm:
+  useEffect(() => {
+    const handleResize = () => {
+      const isSmall = window.innerWidth >= 640
+      setIsSmall(isSmall)
+    }
+    window.addEventListener("resize", handleResize)
+
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
 
   console.log("Properties: ", properties)
   console.log("Server Filters: ", serverFilters)
@@ -63,23 +92,74 @@ const Properties = () => {
 
   return (
     <section className="pb-5">
-      <h1 className="capitalize text-3xl font-semibold">properties</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="capitalize text-3xl font-semibold">properties</h1>
+        {/* Grid and List view Icons */}
+        <div className="flex gap-1 text-primary items-center">
+          <div className="flex group relative my-auto">
+            <Link to={`${location.pathname}/add-property`}>
+              <button className="btn btn-sm text-sm btn-secondary capitalize text-white mx-3 grow-0">
+                {isSmall ? "add property" : <Plus size={iconSize} strokeWidth={"4"} />}
+                {/* <Plus size={iconSize} strokeWidth={"3"} /> */}
+              </button>
+            </Link>
+            {/* tooltip for + button */}
+            {/* <div
+              className={`
+                absolute left-14 bottom-full rounded-md z-30 px-2 py-1 ml-6
+                bg-indigo-100 text-secondary-bright text-sm
+                invisible opacity-20 -translate-y-3 transition-all
+                group-hover:visible group-hover:opacity-100 group-hover:translate-y-0
+              `}
+            >
+              Add Property
+            </div> */}
+          </div>
+          <div
+            className={`${
+              view === LISTVIEW && "bg-secondary-lightest text-secondary-bright"
+            } p-2 cursor-pointer hover:bg-secondary-lightest hover:text-secondary-bright rounded-md`}
+            onClick={() => setView(LISTVIEW)}
+          >
+            <List strokeWidth={strokeWidth} size={iconSize} className="cursor-pointer" />
+          </div>
+          <div
+            className={`${
+              view === GRIDVIEW && "bg-secondary-lightest text-secondary-bright"
+            } p-2 cursor-pointer hover:bg-secondary-lightest hover:text-secondary-bright rounded-md`}
+            onClick={() => setView(GRIDVIEW)}
+          >
+            <LayoutGrid strokeWidth={strokeWidth} size={iconSize} className="cursor-pointer" />
+          </div>
+        </div>
+      </div>
       {/* search and filters */}
-      <div className="flex flex-col md:flex-row justify-between py-3 gap-5">
+      <div className="flex flex-col lg:flex-row justify-between py-3 gap-5">
         <div>
           <Search placeholder={"Search properties"} changeHandler={handleSearch} />
         </div>
         <Filters serverFilters={serverFilters} resetFilters={resetFilters} />
       </div>
-      {/* <p>hello</p> */}
       {isFetching && <Loading />}
       {/* <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-fit place-items-center"> */}
       {/* <section className="grid grid-cols-[repeat(auto-fit,minmax(250px,300px))] gap-4"> */} {/** for complete auto grow do 250px,1fr */}
-      <section className="grid grid-cols-[repeat(auto-fit,minmax(200px,280px))] gap-4 justify-center">
-        {!isFetching && properties?.length > 0
-          ? properties.map((item) => <Property key={item._id} {...item} />)
-          : !isFetching && <h1 className="text-2xl mt-16">No properties available</h1>}
+      {/* properties section */}
+      <section className={`${view === GRIDVIEW ? "grid grid-cols-[repeat(auto-fit,minmax(200px,280px))] gap-4 justify-center" : ""} mb-5`}>
+        {!isFetching && properties?.length > 0 ? (
+          <PropertyRenderer properties={properties} viewType={view} />
+        ) : (
+          !isFetching && <h1 className="text-2xl mt-16">No properties available</h1>
+        )}
       </section>
+      {/* pagination */}
+      {/* <div className="join">
+        <button className="join-item btn active:btn-secondary">Prev</button>
+        {pagination &&
+          times(pagination?.pageCount, (number) => {
+            return <button className="join-item btn active:btn-secondary">{number + 1}</button>
+          })}
+        <button className="join-item btn active:btn-secondary">Next</button>
+      </div> */}
     </section>
   )
 }
