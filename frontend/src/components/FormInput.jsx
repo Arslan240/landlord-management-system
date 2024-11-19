22091ddf20aebc3384e7f135006c609e60e0552a
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-20 02:18:30 +0500

    added select input and checkbox input as well, later i'll separate them.

--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -1,26 +1,80 @@
 import { forwardRef } from "react"
 
-const FormInput = forwardRef(({ label, type, placeholder, disabled, rightLabel, dropdown, ...rest }, ref) => {
-  const padding = dropdown ? `p-2` : `p-4`
+const FormInput = forwardRef(
+  ({ label, type, placeholder, name, disabled, rightLabel, dropdown = false, flexChild = false, small, labelIcon, ...rest }, ref) => {
+    const padding = !dropdown ? `p-[0.6rem]` : `p-4`
+    return (
+      <>
+        <div className={`w-full ${!flexChild && "my-3"}`}>
+          {(label || rightLabel) && (
+            <div className="flex justify-between items-center ">
+              <label htmlFor="" className="block text-sm font-medium mb-1 capitalize">
+                {labelIcon ? (
+                  <div className="flex items-center gap-1">
+                    {label}
+                    {labelIcon}
+                  </div>
+                ) : (
+                  label
+                )}
+              </label>
+              <span className="block mb-2 text-sm text-gray-500 ">{rightLabel}</span>
+            </div>
+          )}
+          {/* {type === "select" && <SelectInput options={options} padding={padding} small={small} />} */}
+          {type !== "select" && (
+            <input
+              name={name}
+              type={type}
+              ref={ref}
+              className={`${padding} block w-full border-gray-200 rounded-lg text-sm focus:border-secondary focus:ring-border-secondary disabled:opacity-50 disabled:pointer-events-none ${
+                small && "text-[0.8rem]"
+              }`}
+              disabled={disabled}
+              placeholder={placeholder}
+              {...rest}
+            />
+          )}
+        </div>
+      </>
+    )
+  }
+)
+export default FormInput
+
+export const SelectInput = ({ label, rightLabel, options, padding, small, flexChild = false, selected }) => {
+  // const padding = !dropdown ? `p-[0.6rem]` : `p-4`
   return (
-    <>
-      <div className="w-full my-3">
+    <div className={`w-full ${!flexChild && "my-3"}`}>
+      {(label || rightLabel) && (
         <div className="flex justify-between items-center ">
-          <label htmlFor="" className="block text-sm font-medium mb-2 capitalize">
+          <label htmlFor="" className="block text-sm font-medium mb-1 capitalize">
             {label}
           </label>
-          <span className="block mb-2 text-sm text-gray-500 ">{rightLabel}</span>
+          {rightLabel && <span className="block mb-2 text-sm text-gray-500 ">{rightLabel}</span>}
         </div>
-        <input
-          type={type}
-          ref={ref}
-          className={`${padding} block w-full border-gray-200 rounded-lg text-sm focus:border-secondary focus:ring-border-secondary disabled:opacity-50 disabled:pointer-events-none`}
-          disabled={disabled}
-          placeholder={placeholder}
-          {...rest}
-        />
-      </div>
-    </>
+      )}
+      <select className={`p-[0.6rem] select select-bordered w-full ${small && "text-[0.8rem]"}`}>
+        <option disabled>Pick one</option>
+        {options.map((option, index) => (
+          <option className="capitalize" value={option.toLowerCase()} selected={index === selected}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
   )
-})
-export default FormInput
+}
+
+export const CheckBoxInput = ({ label, small, name, labelIcon, defaultChecked }) => {
+  return (
+    <div className="form-control flex-row ">
+      <label className="label cursor-pointer ">
+        {/* <input type="checkbox" name={name} defaultChecked className={`checkbox checkbox-accent  ${small ? "checkbox-xs" : "checkbox-sm"}`} /> */}
+        <input name={name} type="checkbox" className="toggle toggle-xs toggle-accent" defaultChecked={defaultChecked} />
+        {labelIcon && <span className="pl-2">{labelIcon}</span>}
+        <span className={`pl-2 label-text ${small && "text-[0.8rem]"}`}>{label}</span>
+      </label>
+    </div>
+  )
+}
