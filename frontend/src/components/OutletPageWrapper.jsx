207dca32466ed8c93cb204c5b8fbda414848eaec
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-20 02:20:46 +0500

    added conditional text sizes for title

--- frontend/src/components/OutletPageWrapper.jsx
+++ frontend/src/components/OutletPageWrapper.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const OutletPageWrapper = ({ children, title = true, showTitle }) => {
   return (
     <section className="pb-5">
-      {title && <h1 className="capitalize text-3xl font-semibold">{title}</h1>}
+      {title && <h1 className={`capitalize text-2xl sm:text-3xl font-semibold mb-2`}>{title}</h1>}
       {children}
     </section>
   )
