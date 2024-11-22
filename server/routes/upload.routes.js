9b6d2304499289e3b825c65099b5b5ab3ad6bac0
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-23 02:54:36 +0500

    fix (upload.routes) auth middleware was missing on upload/getsignedurls, added that

--- server/routes/upload.routes.js
+++ server/routes/upload.routes.js
@@ -4,6 +4,6 @@ const { getPreSignedUrls } = require("../controllers/upload.controller")
 const router = express.Router()
 
 // TODO: add auth-middleware, I only removed it during devlopment
-router.post("/getsignedurls", getPreSignedUrls)
+router.post("/getsignedurls", authMiddleware, getPreSignedUrls)
 
 module.exports = router
