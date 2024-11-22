da6fe953d597ea6bf3f104a46cbbcf7ea6560d69
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-23 02:53:36 +0500

    docs (error handler middleware) added comments to handle a specific E11000 error by mongodb

--- server/middlewares/error-handler.middleware.js
+++ server/middlewares/error-handler.middleware.js
@@ -1,18 +1,26 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
-  const { statusCode, message,data } = err
+  const { statusCode, message, data } = err
 
   console.log(err.statusCode, err.message)
   console.log(err)
-  
+
   // this counts on that there is only one error like this
-  if (message === 'Please verify your email'){ // same message in login controller
+  if (message === "Please verify your email") {
+    // same message in login controller
     return res.status(statusCode || 500).json({
       msg: message,
-      user: data
+      user: data,
     })
   }
 
-  return res.status(statusCode || 500).send({msg:message})
+  return res.status(statusCode || 500).send({ msg: message })
 }
 
 module.exports = errorHandlerMiddleware
+
+// TODO: handle mongodb error of duplicate key
+/**
+ * E11000 duplicate key error collection:
+ * LANDLORD.properties index: address.plotNo_1_address.street_1_address.city_1_address.state_1_address.postalCode_1
+ * dup key: { address.plotNo: "h", address.street: "h", address.city: "h", address.state: "h", address.postalCode: "h" }
+ */
