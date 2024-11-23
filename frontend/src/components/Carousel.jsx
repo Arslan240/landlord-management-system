fce9762678afe275ed957abcd8fc2cad338a3384
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-24 04:04:30 +0500

    fix (Carousel) fixed css bugs and updated to render props instead of children
    changed the css to give proper height to the carousel which solved the issue of showing dots at proper margin from bottom on the properties page. Also changed the styles to only show prev and next buttons on hover over the image

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -2,20 +2,19 @@ import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from "lucide-r
 import { useEffect } from "react"
 import { useState } from "react"
 
-const Carousel = ({ children: slides, autoslide, autoSlideInterval = 2000 }) => {
-
+const Carousel = ({ children, slides, autoslide, autoSlideInterval = 2000 }) => {
   const [curr, setCurr] = useState(0)
 
-  const prev = () => setCurr(prevCurr => ((prevCurr - 1) + slides.length) % slides.length)
+  const prev = () => setCurr((prevCurr) => (prevCurr - 1 + slides.length) % slides.length)
 
-  const next = () => setCurr(prevCurr => (prevCurr + 1) % slides.length)
+  const next = () => setCurr((prevCurr) => (prevCurr + 1) % slides.length)
 
   const handleDots = (id) => {
     setCurr(id)
   }
 
   useEffect(() => {
-    if (!autoslide) return;
+    if (!autoslide) return
     // if want the autoslider to work correctly, use prevState method in setter to access latest state in next and prev.
     const interval = setInterval(() => {
       next()
@@ -23,24 +22,23 @@ const Carousel = ({ children: slides, autoslide, autoSlideInterval = 2000 }) => 
     return () => clearInterval(interval)
   }, [])
 
+  // TODO: assigning relative to outer div here, makes the dots break on properties page. Fix it.
   return (
-    <div>
+    <div className="relative h-full group">
       <div className="overflow-hidden relative w-full h-full ">
-        <div
-          className="flex transition-transform ease-out duration-500 "
-          style={{ transform: `translateX(-${curr * 100}%` }}
-        >
-          {slides}
+        <div className="flex transition-transform duration-500" style={{ transform: `translateX(-${curr * 100}%)` }}>
+          {slides.map((item, index) => (
+            <div key={index} className="w-full flex-shrink-0">
+              <img src={item} alt={`Slide ${index}`} className="w-full h-64 object-cover" />
+            </div>
+          ))}
         </div>
-        <div className="absolute inset-0 flex items-center justify-between p-4">
-          <button onClick={prev}
-            className="p-1 w-7 h-7 flex items-center justify-center bg-gray-200 hover:bg-gray-50 rounded-full"
-          >
+        {/* Arrows */}
+        <div className="absolute inset-0  items-center justify-between p-4 hidden group-hover:flex">
+          <button onClick={prev} className="p-1 w-7 h-7 flex items-center justify-center bg-gray-200 hover:bg-gray-50 rounded-full">
             <ChevronLeft />
           </button>
-          <button onClick={next}
-            className="p-1 w-7 h-7 flex items-center justify-center bg-gray-200 hover:bg-gray-50 rounded-full"
-          >
+          <button onClick={next} className="p-1 w-7 h-7 flex items-center justify-center bg-gray-200 hover:bg-gray-50 rounded-full">
             <ChevronRight />
           </button>
         </div>
@@ -49,8 +47,9 @@ const Carousel = ({ children: slides, autoslide, autoSlideInterval = 2000 }) => 
         <div className="flex gap-2 items-center justify-center">
           {slides.map((_, id) => (
             <div
-              className={`transition-all w-2 h-2 rounded-full bg-slate-100 cursor-pointer ${curr === id ? 'p-1' : 'bg-opacity-50'}`}
+              className={`transition-all w-2 h-2 rounded-full bg-slate-100 cursor-pointer ${curr === id ? "p-1" : "bg-opacity-50"}`}
               key={id}
+              id="dots"
               onClick={() => handleDots(id)}
             />
           ))}
