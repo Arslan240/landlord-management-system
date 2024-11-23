6af735dd52c5b0a07535f83f288070e5b34a902e
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-23 18:50:06 +0500

    docs (customFetch) added a helpful comment to show baseUrl

--- frontend/src/utils/customFetch.js
+++ frontend/src/utils/customFetch.js
@@ -1,8 +1,11 @@
-import axios from "axios";
+import axios from "axios"
 
+/**
+ * @baseURL http://localhost:4000/api/v1/
+ */
 const customFetch = axios.create({
   baseURL: `http://localhost:4000/api/v1/`,
-  withCredentials: true
+  withCredentials: true,
 })
 
 export default customFetch
