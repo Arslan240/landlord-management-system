220df37635437d678448e95ea9bac9b7fcedd6c7
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-25 01:51:55 +0500

    fix (Sidebar) added z index for sidebar item texts, reduced margin for each sidebar item

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -54,7 +54,7 @@ export function SidebarItem({ icon, text, active, alert, to }) {
     <Link
       to={to}
       className={`
-        relative flex items-center py-2 px-3 my-1
+        relative flex items-center py-2 px-3 my-1.5
         font-medium rounded-md cursor-pointer
         transition-colors group
         ${active ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800" : "hover:bg-indigo-50 text-gray-600"}
@@ -67,7 +67,7 @@ export function SidebarItem({ icon, text, active, alert, to }) {
       {!expanded && (
         <div
           className={`
-          absolute left-full rounded-md px-2 py-1 ml-6
+          absolute left-full z-50 rounded-md px-2 py-1 ml-6
           bg-indigo-100 text-secondary-bright text-sm
           invisible opacity-20 -translate-x-3 transition-all
           group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
