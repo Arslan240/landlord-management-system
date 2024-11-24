f685b7e9994f6988e7b31678d02f11b5b618762f
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-24 23:48:07 +0500

    fix (Rent History Model) updated double quotes around refs and changed name of Payment to RentPayment in paymentId

--- server/models/RentHistory.model.js
+++ server/models/RentHistory.model.js
@@ -1,52 +1,49 @@
-const mongoose = require('mongoose')
-const moment = require('moment')
-const { User } = require('./User.model')
-const Property = require('./Property.model')
-const Payment = require('./Payment.model')
+const mongoose = require("mongoose")
+const moment = require("moment")
+const Property = require("./Property.model")
 
 const RentHistorySchema = new mongoose.Schema({
   renterId: {
     type: mongoose.Types.ObjectId,
-    ref: User,
-    default: [true, 'Please provide renterId for RentHistory']
+    ref: "User",
+    default: [true, "Please provide renterId for RentHistory"],
   },
   propertyId: {
     type: mongoose.Types.ObjectId,
     ref: Property,
-    default: [true, 'Please provide propertyId for RentHistory']
+    default: [true, "Please provide propertyId for RentHistory"],
   },
   rentDate: {
     type: Date,
-    required: [true, 'Please provide rent date for RentHistory']
+    required: [true, "Please provide rent date for RentHistory"],
   },
   paymentStatus: {
     type: String,
-    enum: ['paid', 'unpaid'],
-    default: 'unpaid'
+    enum: ["paid", "unpaid"],
+    default: "unpaid",
   },
   paymentId: {
     type: mongoose.Types.ObjectId,
-    ref: Payment,
-    required: [true, 'Please provide paymentId for RentHistory']
-  }
+    ref: "RentPayment",
+    required: [true, "Please provide paymentId for RentHistory"],
+  },
 })
 
 // Define the schema method
 RentHistorySchema.statics.isRentPaidForCurrentMonth = async function (propertyId, renterId) {
-  const startOfMonth = moment().startOf('month').toDate();
-  const endOfMonth = moment().endOf('month').toDate();
+  const startOfMonth = moment().startOf("month").toDate()
+  const endOfMonth = moment().endOf("month").toDate()
 
   const rentRecord = await this.findOne({
     propertyId,
     renterId,
     rentDate: { $gte: startOfMonth, $lte: endOfMonth },
-    paymentStatus: 'paid',
-  });
+    paymentStatus: "paid",
+  })
 
-  return !!rentRecord; // returns true if a record is found (rent is paid)
-};
+  return !!rentRecord // returns true if a record is found (rent is paid)
+}
 
-
-const RentHistory = mongoose.model('RentHistory', RentHistorySchema)
+const RentHistory = mongoose.model("RentHistory", RentHistorySchema)
 
 module.exports = RentHistory
