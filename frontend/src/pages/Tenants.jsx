2398bff1c9a5846f8eb27e8d9ecc6963af54919a
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-27 02:31:17 +0500

    feat (Tenants) added a new button add tenant

--- frontend/src/pages/Tenants.jsx
+++ frontend/src/pages/Tenants.jsx
@@ -1,9 +1,15 @@
 import React from "react"
 import OutletPageWrapper from "../components/OutletPageWrapper"
 import RequireAuth from "../components/RequireAuth"
+import { Link } from "react-router-dom"
 
 const Tenants = () => {
-  return <OutletPageWrapper title={"Tenants"}></OutletPageWrapper>
+  const button = (
+    <Link to={`/dashboard/tenants/add-tenant`}>
+      <button className="btn text-white btn-secondary btn-sm">Add Tenant</button>
+    </Link>
+  )
+  return <OutletPageWrapper title={"Tenants"} button={button}></OutletPageWrapper>
 }
 
 export default RequireAuth(Tenants)
