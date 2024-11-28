4b9ecacb3707145e70b90bc80b43e56235343d3b
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-28 15:28:27 +0500

    docs (Maintenance Page) just some guiding comments on how to develope the page

--- frontend/src/pages/Maintenance.jsx
+++ frontend/src/pages/Maintenance.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import OutletPageWrapper from "../components/OutletPageWrapper"
 import RequireAuth from "../components/RequireAuth"
 
+// 1- First section - one for requests on properties maintained by the current user (including requests made by the user as a landlord)
+// 2- Second section - one for requests made by current user on other properties which user might have rented before
 const Maintenance = () => {
   return <OutletPageWrapper title={"Maintenance"}></OutletPageWrapper>
 }
