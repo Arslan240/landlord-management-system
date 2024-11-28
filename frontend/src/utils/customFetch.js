1e14d499bbd34daf0029ce15e9997d54d5fb53ba
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-28 13:51:36 +0500

    feat (custom fetch) added an axios interceptor to handle 401 error centrally and logout user
    centralized error handling in react like we did in our server using error handler middleware

--- frontend/src/utils/customFetch.js
+++ frontend/src/utils/customFetch.js
@@ -1,4 +1,7 @@
 import axios from "axios"
+import { store } from "../redux/store"
+import { logout } from "../redux/userSlice"
+import { toast } from "react-toastify"
 
 /**
  * @baseURL http://localhost:4000/api/v1/
@@ -8,4 +11,15 @@ const customFetch = axios.create({
   withCredentials: true,
 })
 
+customFetch.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response && error.response.status === 401) {
+      toast.error("Session Expired. Please login again")
+      store.dispatch(logout())
+      window.location.href("/login")
+    }
+    return Promise.reject(error)
+  }
+)
 export default customFetch
