e022a1b87351917b136eeb83c225cc09369bae0e
Arslan240 <arslan.jamil1322@gmail.com>
2024-11-28 10:09:13 +0500

    fix (Require Auth) put toast in useEffect, solved error of Cannot update a component when another component is rendering

--- frontend/src/components/RequireAuth.jsx
+++ frontend/src/components/RequireAuth.jsx
@@ -1,24 +1,24 @@
 import { Navigate } from "react-router-dom"
 import { useUserState } from "../redux/userSlice"
 import { toast } from "react-toastify"
+import { useEffect } from "react"
 
-function RequireAuth(Component, redirectTo='/login'){
-  return function ProtectedComponent(props){
+function RequireAuth(Component, redirectTo = "/login") {
+  return function ProtectedComponent(props) {
     const user = useUserState()
     const isLoggedIn = user?.name ? true : false // we could also just check for user ? , but to confirm checking for user.
 
-    if (!isLoggedIn){
-      toast.error('You\'re not logged in. Please log in first.')
-    }
-    
-    return isLoggedIn ? <Component {...props}/> : <Navigate to={redirectTo} />  
+    useEffect(() => {
+      if (!isLoggedIn) {
+        toast.error("You're not logged in. Please log in first.")
+      }
+    }, [isLoggedIn])
+
+    return isLoggedIn ? <Component {...props} /> : <Navigate to={redirectTo} />
   }
 }
 
 export default RequireAuth
-
-
-
 
 // example implementatino
 
