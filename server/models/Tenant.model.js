97d305bcc2ebf31441b5e25a7740a012f41ff3ba
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-04 00:53:03 +0500

    feat (Tenant Model) added new model for storing tenants

---
+++ server/models/Tenant.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose")
+
+const TenantSchema = new mongoose.Schema(
+  {
+    user: {
+      type: String,
+      ref: "User",
+      default: null, // null for offline tenant (i.e not a registered user on rently)
+    },
+    name: {
+      type: String,
+      required: [true, "Please provide name for tenant"],
+    },
+    dob: {
+      type: Date,
+      required: [true, "Please provide date of birth for tenant"],
+    },
+    occupation: {
+      type: Date,
+      required: [true, "Please provide occupation of the tenant"],
+    },
+    idNumber: {
+      type: String,
+      unique: true,
+      required: [true, "Please provide govt identification number"],
+    },
+    salary: Number,
+    imageUrl: String,
+    isOffline: {
+      type: Boolean,
+      default: true, // true if tenant is offline
+    },
+  },
+  { timestamps: true }
+)
+
+TenantSchema.indexes({ idNumber: 1 }, { unique: true })
