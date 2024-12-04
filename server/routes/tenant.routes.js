d3e44de3674769dbdd718c17a1f683ec599598fe
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-05 01:20:46 +0500

    docs (tenant.routes) added some comments for future things to implement

--- server/routes/tenant.routes.js
+++ server/routes/tenant.routes.js
@@ -3,6 +3,9 @@ const { authMiddleware } = require("../middlewares/auth.middleware")
 const { getTenants, addTenant } = require("../controllers/tenant.controller")
 const router = express.Router()
 
+/* TODO: add roles middleware to only allow landlord to access these routes.
+ * also on frontend in leases and tenants page check, when first property is added add the role of landlord to the user.
+ */
 router.route("/").get(authMiddleware, getTenants)
 router.route("/").post(authMiddleware, addTenant)
 
