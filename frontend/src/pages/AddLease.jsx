72a88f9e805c10ea6b2aae5bf13b17893e7b634e
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-06 12:30:34 +0500

    fix (AddLease): updated to use FormWrapper and TenantDetailsForm are used here

--- frontend/src/pages/AddLease.jsx
+++ frontend/src/pages/AddLease.jsx
@@ -1,10 +1,25 @@
-import React from "react"
+import FormWrapper from "../components/FormWrapper"
+import { useSearchParams } from "react-router-dom"
 import OutletPageWrapper from "../components/OutletPageWrapper"
 import RequireAuth from "../components/RequireAuth"
+import { AnimatePresence } from "motion/react"
+import { TenantDetailsForm } from "../components/AddLeaseForm/"
+import { useState } from "react"
 
 const AddLease = () => {
   // in property dropdown, when you select a property then navigate to the page where property id
-  return <OutletPageWrapper title={"Create New Lease"}></OutletPageWrapper>
+  const searchParams = useSearchParams()
+  const { prop, redirect } = searchParams
+  const [formState, setFormState] = useState({ tenantDetails: { isOffline: true }, propertyDetails: {} })
+  return (
+    <OutletPageWrapper title={"Create New Lease"}>
+      <FormWrapper width="100%">
+        <AnimatePresence>
+          <TenantDetailsForm />
+        </AnimatePresence>
+      </FormWrapper>
+    </OutletPageWrapper>
+  )
 }
 
 export default RequireAuth(AddLease)
