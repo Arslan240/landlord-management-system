5af7c39402ba1199336af8edf39ce74a3c58dd51
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-06 12:29:30 +0500

    feat (Tenant Details): added a new form to collect tenant details for add lease page

---
+++ frontend/src/components/AddLeaseForm/TenantDetailsForm.jsx
@@ -0,0 +1,133 @@
+import { motion } from "motion/react"
+import { useForm } from "react-hook-form"
+import FormInput from "../FormInput"
+import CheckBoxInput from "../CheckBoxInput"
+import Dropzone from "../Dropzone"
+import { toast } from "react-toastify"
+import { useState } from "react"
+import { validateDate } from "../../utils/validateDate"
+
+// for time being we don't need multi steps in the form so we'll start by simple forms without animation and steps
+const TenantDetailsForm = () => {
+  const [acceptedFiles, setAcceptedFiles] = useState([])
+
+  const {
+    register,
+    formState: { errors, isLoading, isSubmitSuccessful },
+    watch,
+    handleSubmit,
+  } = useForm({
+    defaultValues: { isOffline: true },
+    mode: "onBlur",
+  })
+
+  const offlineTenant = watch("isOffline")
+
+  const onSubmit = (data) => {
+    // e.preventDefault()
+    console.log(offlineTenant)
+    console.log(data)
+    console.log("acceptedFiles: ", acceptedFiles)
+  }
+
+  return (
+    <form onSubmit={handleSubmit((data) => onSubmit(data))}>
+      <h1 className="text-2xl font-medium">Tenant Details</h1>
+      <CheckBoxInput label={"Is tenant offline?"} name={"isOffline"} {...register("isOffline")} error={errors["isOffline"]} />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+        <FormInput
+          label={"Name"}
+          type={"text"}
+          name={"name"}
+          flexChild
+          placeholder={"John Jackie"}
+          small
+          {...register("name", {
+            required: "Please provide name of tenant",
+          })}
+          error={errors["name"]}
+        />
+        {offlineTenant ? (
+          <>
+            <FormInput
+              label={"Occupation"}
+              type={"text"}
+              name={"occupation"}
+              small
+              placeholder={"Engineering Manager"}
+              {...register("occupation", {
+                required: "Please provide occupation of tenant",
+              })}
+              error={errors["occupation"]}
+              flexChild
+            />
+            <FormInput
+              label={"Date of Birth"}
+              type={"text"}
+              name={"dob"}
+              small
+              placeholder={"DD/MM/YYYY"}
+              {...register("dob", {
+                required: "Please provide date of birth",
+                validate: validateDate,
+              })}
+              error={errors["dob"]}
+              flexChild
+            />
+            <FormInput
+              label={"Govt identification number"}
+              type={"text"}
+              name={"idNumber"}
+              small
+              placeholder={"XXHJHDJJDJU"}
+              {...register("idNumber", {
+                required: "Please provide govt identification number",
+              })}
+              error={errors["idNumber"]}
+              flexChild
+            />
+            <FormInput
+              label={"Annual Salary"}
+              type={"text"}
+              name={"salary"}
+              small
+              placeholder={"45000"}
+              {...register("salary")}
+              error={errors["salary"]}
+              flexChild
+            />
+            <div>
+              <label className="font-medium mt-3 inline-block">Tenant Image</label>
+              <Dropzone
+                acceptedFiles={acceptedFiles}
+                setAcceptedFiles={setAcceptedFiles}
+                maxFiles={1}
+                maxFilesMessage={"You can only upload 1 image"}
+                minimalist
+              />
+            </div>
+          </>
+        ) : (
+          <FormInput
+            label={"Email"}
+            type={"email"}
+            name={"email"}
+            placeholder={"John Jackie"}
+            flexChild
+            small
+            {...register("email", {
+              required: "Please provide email of tenant",
+            })}
+            error={errors["email"]}
+          />
+        )}
+      </div>
+      <button type="submit" className="btn btn-secondary btn-sm text-white mt-4">
+        Submit
+      </button>
+    </form>
+  )
+}
+
+export default TenantDetailsForm
---
+++ frontend/src/components/AddLeaseForm/index.js
@@ -0,0 +1,1 @@
+export { default as TenantDetailsForm } from "./TenantDetailsForm"
