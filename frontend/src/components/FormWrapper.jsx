36796ba5fa9f42ab8c0d589f7b59625f6148bb04
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-06 12:25:41 +0500

    feat (FormWrapper) added a simple wrapper for forms to apply basic padding and margins

---
+++ frontend/src/components/FormWrapper.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const FormWrapper = ({ children, width = "400px" }) => {
+  const formWidth = `max-w-[${width}]`
+  return (
+    <div className={`${formWidth} mx-auto`}>
+      <div className="bg-primary-lightest px-7 py-7 rounded-lg">{children}</div>
+    </div>
+  )
+}
+
+export default FormWrapper
