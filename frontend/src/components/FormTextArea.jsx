63ba54be948c5a86a0b090df70280b9744fd21a6
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-07 15:08:32 +0500

    fix (FormTextArea): update reusable textarea to have labelicon and rightlabel as well, just like other reusable labels

--- frontend/src/components/FormTextArea.jsx
+++ frontend/src/components/FormTextArea.jsx
@@ -1,9 +1,23 @@
 import React from "react"
 
-const FormTextArea = ({ label, name, placeholder, small, error, ...rest }) => {
+const FormTextArea = ({ label, rightLabel, labelIcon, name, placeholder, small, error, ...rest }) => {
   return (
     <div>
-      <label htmlFor={`${label}-${name}`}>{label}</label>
+      {(label || rightLabel) && (
+        <div className="flex justify-between items-center ">
+          <label htmlFor={`${label}-${name}`} className="block text-sm font-medium mb-1 capitalize">
+            {labelIcon ? (
+              <div className="flex items-center gap-1">
+                {label}
+                {labelIcon}
+              </div>
+            ) : (
+              <>{label}</>
+            )}
+          </label>
+          <span className="block mb-2 text-sm text-gray-500 ">{rightLabel}</span>
+        </div>
+      )}
       <textarea className={`w-full h-36 p-2 ${small && "text-xs"}`} id={`${label}-${name}`} name={name} {...rest} />
       {error && <p className={`text-warning py-1 ${small && "text-xs"}`}>{error.message}</p>}
     </div>
