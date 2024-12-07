c820ad79ffc93935dd69f93eded9d0604aa8fbca
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-08 01:19:03 +0500

    docs (Lease routes): just added a small task of adding additional middleware to lease routes in future

--- server/routes/lease.routes.js
+++ server/routes/lease.routes.js
@@ -3,6 +3,7 @@ const { getLeases, addLease } = require("../controllers/lease.controller")
 const { authMiddleware } = require("../middlewares/auth.middleware")
 const router = express.Router()
 
+// TODO: add authorizeRoles middleware as well, only landlords can access this route
 router
   .route("/")
   .get(authMiddleware, getLeases) //get all leases
