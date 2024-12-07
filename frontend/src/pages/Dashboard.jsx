7b0f6f4bb71b4bef42dab8afd2669a2fbf7ea9d2
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-08 01:44:37 +0500

    fix (Dashboard page): tried to add width classes using useEffects
    also added conditional widths based on url routes, did't work out great, but i'm keeping it for now

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,10 @@
-import { Outlet, useNavigation } from "react-router-dom"
+import { Outlet, useLocation, useNavigation } from "react-router-dom"
 import Header from "../components/Header"
 import RequireAuth from "../components/RequireAuth"
 import Sidebar from "../components/Sidebar"
 import SidebarRenderer from "../components/SidebarRenderer"
 import Breadcrumbs from "../components/Breadcrumbs"
-import { createContext, useContext, useRef, useState } from "react"
+import { createContext, useContext, useEffect, useRef, useState } from "react"
 import Loading from "../components/Loading"
 
 const SidebarContext = createContext()
@@ -17,6 +17,45 @@ const Dashboard = ({ children }) => {
   const navigation = useNavigation()
   const isLoading = navigation.state === "loading"
   const outletRef = useRef()
+
+  const location = useLocation()
+  const [widthClass, setWidthClass] = useState("w-full")
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  const calculateWidthClass = () => {
+    const isAddLease = location.pathname.includes("/add-lease")
+    const sm = 640
+    const md = 768
+    const lg = 1024
+    const xl = 1280
+    const xl2 = 1536
+
+    if (windowWidth < sm) {
+      return "w-full"
+    }
+
+    if (isAddLease) return "w-full"
+
+    if (expanded) return "md:w-full lg:w-full xl:w-full"
+  }
+
+  // width setter useEffect
+  useEffect(() => {
+    setWidthClass(calculateWidthClass())
+  }, [location.pathname, expanded, windowWidth])
+
+  // resize listener
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+    }
+
+    window.addEventListener("resize", handleResize)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [])
 
   return (
     <>
@@ -32,11 +71,12 @@ const Dashboard = ({ children }) => {
             <Loading />
           ) : (
             <main
-              className={`flex-grow padding dashboard-outlet transition-all delay-[50] text-sm w-full  
-                ${expanded && "w-full"} sm:w-[85%] 
+              className={`flex-grow padding dashboard-outlet transition-all delay-[50] text-sm w-full
+                ${expanded && "w-full"} sm:w-[85%]
                 md:${expanded ? "w-full" : "w-[85%]"} lg:${expanded ? "w-full" : "w-[85%]"} xl:${
                 expanded ? "w-full" : "w-[85%]"
-              } 3xl:w-[75%] mx-auto overflow-y-auto max-w-[85rem] `}
+              } 2xl:w-[75%] mx-auto overflow-y-auto max-w-[85rem]`}
+              // className={`flex-grow padding dashboard-outlet transition-all delay-[50] text-sm ${widthClass} 2xl-w[75%] mx-auto overflow-y-auto max-w-[85rem]`}
             >
               <Outlet />
             </main>
