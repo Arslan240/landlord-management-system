633e9cf87c3521283e086805125e2685a5c9ce2e
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-07 14:25:14 +0500

    fix (FormWrapper): updated padding for mobile and larger screens

--- frontend/src/components/FormWrapper.jsx
+++ frontend/src/components/FormWrapper.jsx
@@ -4,7 +4,7 @@ const FormWrapper = ({ children, width = "400px" }) => {
   const formWidth = `max-w-[${width}]`
   return (
     <div className={`${formWidth} mx-auto`}>
-      <div className="bg-primary-lightest px-7 py-7 rounded-lg">{children}</div>
+      <div className="bg-primary-lightest px-7 py-5 sm:py-7 rounded-lg">{children}</div>
     </div>
   )
 }
