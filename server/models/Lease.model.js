0ed48826e73fdfe8122c83131f5e429ae4e45f38
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-09 02:22:01 +0500

    feat (Lease Model): updated to add one more status of Lease

--- server/models/Lease.model.js
+++ server/models/Lease.model.js
@@ -2,16 +2,18 @@ const mongoose = require("mongoose")
 
 const leaseSchema = new mongoose.Schema({
   tenantId: {
+    // tenant idnot required because we create a lease first for online tenant if he accepts the request then we update the tenantId here.
+    // Also when a user is not tenant already but accepts the lease, then he will be directed to a new page where he will complete his info about becoming a tenant.
+    // Then tenant will be created with similar userid and tenant id will be passed to add lease to change status to accepted.
     type: mongoose.Types.ObjectId,
     ref: "User",
-    required: [true, "Please provide appropriate renter Id"],
   },
   propertyId: {
     type: mongoose.Types.ObjectId,
     ref: "Property",
     required: [true, "Please provide appropriate property Id"],
   },
-  rentAmount: {
+  rent: {
     type: Number,
     required: [true, "Please provide appropriate number"],
   },
@@ -22,6 +24,12 @@ const leaseSchema = new mongoose.Schema({
   deposit: Number,
   endDate: Date, //if no end date it means the lease will be on going for near future.
   terms: String,
+  status: {
+    // rejected: tenant rejects, cancelled: landlord withdraws, if already rejected / cancelled, you can't change the status
+    type: String,
+    enum: ["pending", "accepted", "rejected", "cancelled"],
+    default: "pending",
+  },
 })
 
 const Lease = mongoose.model("Lease", leaseSchema)
