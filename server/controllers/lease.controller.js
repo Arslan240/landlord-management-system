253548901649bc88b0f16fdd05e379edee97d0f9
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-09 02:23:40 +0500

    feat (Lease controller): added a the add lease controller, including validation,ownership and other checks etc

--- server/controllers/lease.controller.js
+++ server/controllers/lease.controller.js
@@ -1,8 +1,55 @@
+const { StatusCodes } = require("http-status-codes")
+const { BadRequestError, UnAuthenticateError, NotFoundError } = require("../errors")
+const { User } = require("../models/User.model")
+const Lease = require("../models/Lease.model")
+const Property = require("../models/Property.model")
+
 const getLeases = async (req, res) => {
   res.send("Get Leases")
 }
 const addLease = async (req, res) => {
+  const { tenantDetails, propertyDetails } = req.body
+  const { isOffline, name, email, idNumber, occupation, dob, salary } = tenantDetails
+  const { propertyId, rent, deposit, startDate, endDate, terms } = propertyDetails
+
+  if (!propertyId || !rent || !startDate) {
+    throw new BadRequestError("Please provide Property, Rent and startDate for lease")
+  }
+
+  // if online tenant, send email of lease request to user's email
+  if (!isOffline) {
+    if (!email || !name || !idNumber || !occupation) {
+      throw new BadRequestError("Please provide Name, Email, Occupation and Govt Id of tenant")
+    }
+
+    const tenant = await User.findOne({ email, idNumber })
+    if (!tenant) {
+      throw new BadRequestError(`Tenant with ${email} and Govt Id ${idNumber} doesn't exist. Please correct tenant details and submit again`)
+    }
+
+    const propertyOwns = await Property.findOne({ _id: propertyId })
+    if (!propertyOwns) {
+      throw new NotFoundError("Property not found. Please check your details")
+    }
+
+    if (propertyOwns.owner !== req.user.id) {
+      // invalidate the refresh token first
+      throw new UnAuthenticateError("Access denied. Please verify your credentials")
+    }
+
+    const lease = await Lease.create({ propertyId, rent, deposit, startDate, endDate, terms })
+    if (!lease) {
+      console.log("Lease not created in lease.controller.js for offline tenant")
+      throw new Error("Something went wrong")
+    }
+  }
+
+  // if tenant is offline and we need to call add tenant controller from here, then call add lease controller with property id and newly created tenant id
+  // landlord should own the property, if not then unauthorised error, also maybe invalid the refresh token, and logout the user.
+  // if already rejected / cancelled, you can't change the status
+
   res.send({ msg: "Add new Lease", body: req.body })
+  // return res.status(StatusCodes.OK).json({ msg: `Email sent successfully to ${name} at ${email}. Please wait for acceptance of lease.` })
 }
 
 module.exports = {
