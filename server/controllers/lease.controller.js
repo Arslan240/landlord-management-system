bff6820a202dd1a11e3916ee86da10856385a698
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-09 10:13:04 +0500

    refactor (Add Lease controller): extracted online tenant lease functionality in to a separate function

--- server/controllers/lease.controller.js
+++ server/controllers/lease.controller.js
@@ -19,32 +19,7 @@ const addLease = async (req, res) => {
 
   // if online tenant, send email of lease request to user's email
   if (!isOffline) {
-    if (!email || !name || !idNumber || !occupation) {
-      throw new BadRequestError("Please provide Name, Email, Occupation and Govt Id of tenant")
-    }
-
-    const tenant = await User.findOne({ email, idNumber })
-    if (!tenant) {
-      throw new BadRequestError(`Tenant with ${email} and Govt Id ${idNumber} doesn't exist. Please correct tenant details and submit again`)
-    }
-
-    const propertyOwns = await Property.findOne({ _id: propertyId })
-    if (!propertyOwns) {
-      throw new NotFoundError("Property not found. Please check property details")
-    }
-
-    if (propertyOwns.owner !== req.user.id) {
-      throw new UnAuthenticateError("Access denied. Please verify your credentials")
-    }
-
-    const lease = await Lease.create({ propertyId, rent, deposit, startDate, endDate, terms })
-    if (!lease) {
-      throw new Error("Lease didn't create, Please try again")
-    }
-
-    await sendLeaseAcceptanceEmail({ email, name, leaseDetails: { address: propertyOwns.address, leaseId: lease._id } })
-    console.log("Accept lease email sent successfully")
-    res.status(StatusCodes.CREATED).json({ msg: `Lease request is sent to ${name}. Please wait patiently, you'll be notified on acceptance.` })
+    await onlineTenantLeaseController({ tenantDetails, propertyDetails })
   }
 
   // if tenant is offline and we need to call add tenant controller from here, then call add lease controller with property id and newly created tenant id
@@ -58,4 +33,36 @@ const addLease = async (req, res) => {
 module.exports = {
   getLeases,
   addLease,
+}
+
+async function onlineTenantLeaseController({ propertyDetails, tenantDetails }) {
+  const { name, email, idNumber, occupation } = tenantDetails
+  const { propertyId, rent, deposit, startDate, endDate, terms } = propertyDetails
+
+  if (!email || !name || !idNumber || !occupation) {
+    throw new BadRequestError("Please provide Name, Email, Occupation and Govt Id of tenant")
+  }
+
+  const tenant = await User.findOne({ email, idNumber })
+  if (!tenant) {
+    throw new BadRequestError(`Tenant with ${email} and Govt Id ${idNumber} doesn't exist. Please correct tenant details and submit again`)
+  }
+
+  const propertyOwns = await Property.findOne({ _id: propertyId })
+  if (!propertyOwns) {
+    throw new NotFoundError("Property not found. Please check property details")
+  }
+
+  if (propertyOwns.owner !== userId) {
+    throw new UnAuthenticateError("Access denied. Please verify your credentials")
+  }
+
+  const lease = await Lease.create({ propertyId, rent, deposit, startDate, endDate, terms })
+  if (!lease) {
+    throw new Error("Lease didn't create, Please try again")
+  }
+
+  await sendLeaseAcceptanceEmail({ email, name, leaseDetails: { address: propertyOwns.address, leaseId: lease._id } })
+  console.log("Accept lease email sent successfully")
+  res.status(StatusCodes.CREATED).json({ msg: `Lease request is sent to ${name}. Please wait patiently, you'll be notified on acceptance.` })
 }
