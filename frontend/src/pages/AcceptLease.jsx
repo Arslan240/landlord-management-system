485d583c8abce8b839d870edeca8971b7b049e71
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-12 01:35:40 +0500

    fix (Accept Lease): updated to add loader and page wrapper on the accept lease page

--- frontend/src/pages/AcceptLease.jsx
+++ frontend/src/pages/AcceptLease.jsx
@@ -1,16 +1,27 @@
 import { useLoaderData } from "react-router-dom"
+import { fetchLease } from "../queries/useFetchLease"
+import OutletPageWrapper from "../components/OutletPageWrapper"
 
 export const acceptLeaseLoader =
   (queryClient) =>
-  async ({ request }) => {
-    return queryClient.fetchQuery()
+  async ({ params }) => {
+    try {
+      const { id: leaseId } = params
+      return queryClient.fetchQuery({
+        queryKey: ["leases", leaseId],
+        queryFn: fetchLease(leaseId),
+      })
+    } catch (error) {
+      toast.error(error.message)
+      return error
+    }
   }
 
 const AcceptLease = () => {
   const data = useLoaderData()
   console.log("accept lease data: ", data)
 
-  return <div>AcceptLease</div>
+  return <OutletPageWrapper showTitle={"Accept Lease"}></OutletPageWrapper>
 }
 
 export default AcceptLease
