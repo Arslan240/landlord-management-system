6c19487afa35cd287db0e68c3501ea6708e16358
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-12 22:55:16 +0500

    fix (get Error Message): updated the error extraction to also check for statusText in response

--- frontend/src/utils/getErrorMessage.js
+++ frontend/src/utils/getErrorMessage.js
@@ -4,7 +4,7 @@ const getErrorMessage = (error) => {
     return
   }
   const { response } = error
-  const errorMessage = response?.data?.msg || error.message || "Something went wrong, Please try again"
+  const errorMessage = response?.data?.msg || response?.statusText || error.message || "Something went wrong, Please try again"
   return errorMessage
 }
 
