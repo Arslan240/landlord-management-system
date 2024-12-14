d24de7c92d0ba6f3b9d61d98b32ad8493ce3f8a3
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-14 16:32:09 +0500

    feat (dummy tenant values): dummy values while property doesn't have tenants and leases and requests

---
+++ frontend/src/tenantValues.js
@@ -0,0 +1,30 @@
+export const tenantValues = [
+  [
+    { type: "avatar", value: "Joseph" },
+    { value: "joseph@gmail.com" },
+    { value: "12 Dec, 2029" },
+    { value: "$299" },
+    { type: "badge", value: "available" },
+  ],
+  [
+    { type: "avatar", value: "Emily" },
+    { value: "emily@gmail.com" },
+    { value: "19 March, 2028" },
+    { value: "$800" },
+    { type: "badge", value: "occupied" },
+  ],
+  [
+    { type: "avatar", value: "Abraham" },
+    { value: "abraham@gmail.com" },
+    { value: "23 Jan, 2030" },
+    { value: "$340" },
+    { type: "badge", value: "available" },
+  ],
+  [
+    { type: "avatar", value: "Hailey" },
+    { value: "hailey@gmail.com" },
+    { value: "9 August, 2025" },
+    { value: "$670" },
+    { type: "badge", value: "occupied" },
+  ],
+]
