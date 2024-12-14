6cfa4552b779239c693cfc071bf1ac913ea72e92
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-14 16:41:14 +0500

    feat (Leases page): added a button to add-lease url

--- frontend/src/pages/Leases.jsx
+++ frontend/src/pages/Leases.jsx
@@ -1,9 +1,16 @@
 import React from "react"
 import OutletPageWrapper from "../components/OutletPageWrapper"
 import RequireAuth from "../components/RequireAuth"
+import { Link } from "react-router-dom"
+
+const button = (
+  <Link to={"/dashboard/leases/add-lease"}>
+    <button className="btn text-white btn-secondary btn-sm">Add Lease</button>
+  </Link>
+)
 
 const Leases = () => {
-  return <OutletPageWrapper title={"Leases"}></OutletPageWrapper>
+  return <OutletPageWrapper title={"Leases"} button={button}></OutletPageWrapper>
 }
 
 export default RequireAuth(Leases)
