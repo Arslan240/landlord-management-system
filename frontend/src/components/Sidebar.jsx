bd5df8ba8a36ecacaa4e0d129432c7f6aba4afb6
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-17 22:11:08 +0500

    feat (Sidebar): added animated sub menus of menu items
    used grid-template-rows: 0fr and overflow-hidden and when opened, we set grid-template-rows: 1fr, by using this way, we can easily animate height of container, in other ways using height:auto transition doesn't work.

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -20,7 +20,7 @@ export default function Sidebar({ children }) {
               alt=""
             /> */}
           <button onClick={() => setExpanded((curr) => !curr)} className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100 ">
-            <ChevronsLeft className={`transition-all ${!expanded ? "rotate-180" : "rotate-0"}`} />
+            <ChevronsLeft className={`transition-all delay-300 ${!expanded ? "rotate-180" : "rotate-0"}`} />
           </button>
         </div>
 
@@ -63,13 +63,17 @@ export function SidebarItem({ icon, text, active, alert = false, to, subRoutes, 
   return (
     <>
       <div
-        className={`relative flex justify-between items-center ${small ? "py-2 my-1" : "py-2.5 my-1.5"} px-3  transition-colors group rounded-md ${
+        className={`relative flex justify-between items-center ${
+          small ? "py-2 my-1 text-xs" : "py-2.5 my-1.5"
+        } px-3  transition-colors group rounded-md ${
           active ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800" : "hover:bg-indigo-50 text-gray-600"
         }`}
       >
         <Link to={to} className={`relative flex items-center font-medium cursor-pointer w-full`}>
           {icon}
-          <span className={`text-sm font-normal overflow-hidden transition-all ${expanded ? "w-32 ml-3" : "w-0"}`}>{text}</span>
+          <span className={`${small ? "text-[0.83rem]" : "text-sm"} font-normal overflow-hidden transition-all ${expanded ? "w-32 ml-3" : "w-0"}`}>
+            {text}
+          </span>
           {alert && <div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? "" : "top-2"}`} />}
 
           {!expanded && (
@@ -108,18 +112,12 @@ const SubMenu = ({ isSubExpanded, menuItems }) => {
   if (!menuItems || menuItems?.length <= 0) return
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: -10 }} // Start hidden and slightly above
-      animate={{
-        opacity: isSubExpanded ? 1 : 0,
-        y: isSubExpanded ? 0 : -10, // Slide down/up effect
-      }}
-      transition={{ duration: 0.2, ease: "easeInOut" }}
-      className={`overflow-hidden pl-2 ${isSubExpanded ? "block" : "hidden"}`}
-    >
-      {menuItems.map((item) => (
-        <SidebarItem {...item} small />
-      ))}
-    </motion.div>
+    <div className={`grid ${isSubExpanded ? "grid-rows-[1fr]" : "grid-rows-[0fr]"} transition-all delay-150 ease-in-out`}>
+      <div className="overflow-hidden">
+        {menuItems.map((item) => (
+          <SidebarItem {...item} small />
+        ))}
+      </div>
+    </div>
   )
 }
