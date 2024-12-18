c9752a7d8e8d76bb61cddfe295c6ecb711b7a3d0
Arslan240 <arslan.jamil1322@gmail.com>
2024-12-19 01:03:25 +0500

    feat (Accept Lease): added image carousel and changed some layout

--- frontend/src/pages/AcceptLease.jsx
+++ frontend/src/pages/AcceptLease.jsx
@@ -5,6 +5,8 @@ import { fetchLease } from "../queries/useFetchLease"
 import OutletPageWrapper from "../components/OutletPageWrapper"
 import { getAddressFromObject, getErrorMessage } from "../utils"
 import { useUserState } from "../redux/userSlice"
+import { CLOUDFRONT } from "../constants"
+import Carousel from "../components/Carousel"
 
 export const acceptLeaseLoader =
   (queryClient) =>
@@ -19,7 +21,7 @@ export const acceptLeaseLoader =
     } catch (error) {
       const errorMessage = getErrorMessage(error)
       toast.error(errorMessage)
-      // return error
+      return error
     }
   }
 
@@ -29,7 +31,8 @@ const AcceptLease = () => {
   const data = useLoaderData()
   const { name } = useUserState()
   const { landlord, property, startDate, endDate, rent, deposit } = data
-  const { name: propertyName } = property
+  const { name: propertyName, images } = property
+  const propertyImages = images.map((imageId) => `${CLOUDFRONT}/${imageId}`)
 
   const start = moment(startDate).format("MMMM do, YYYY")
   const end = endDate ? moment(endDate).format("MMMM do, YYYY") : null
@@ -39,6 +42,9 @@ const AcceptLease = () => {
   const address = getAddressFromObject(property.address)
   const title = propertyName || address
 
+  const acceptHandler = async () => {}
+  const rejectHandler = async () => {}
+
   return (
     <OutletPageWrapper title={"Accept Lease"}>
       <div className="px-2 py-4">
@@ -46,49 +52,58 @@ const AcceptLease = () => {
         <p>
           Hi, {name}. Your landlord {landlord.name} has requested you to accept this lease.
         </p>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-          <div>
-            <h2 className="mt-2 text-xl font-extrabold">Property</h2>
-            <p>
-              {propertyName && (
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="md:w-2/5 ">
+            <Carousel slides={propertyImages} roundedClass={`rounded-xl`} imageHeightClass={"h-[14rem] lg:h-[15rem]"} spaceBetweenImages thumbs />
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 md:gap-2">
+            <div>
+              <h2 className="mt-2 text-xl font-extrabold">Property</h2>
+              <div>
+                {propertyName && (
+                  <p>
+                    <span className="font-semibold">Name: </span>
+                    <span>{propertyName}</span>
+                  </p>
+                )}
                 <p>
-                  <span className="font-semibold">Name: </span>
-                  <span>{propertyName}</span>
+                  <span className="font-semibold">Address: </span>
+                  <span>{address}</span>
                 </p>
-              )}
+              </div>
+            </div>
+            <div>
+              <h2 className="mt-2 text-xl font-extrabold">Lease </h2>
               <p>
-                <span className="font-semibold">Address: </span>
-                <span>{address}</span>
+                <span className="font-semibold">Start: </span>
+                <span>{start} </span>
               </p>
-            </p>
-          </div>
-          <div>
-            <h2 className="mt-2 text-xl font-extrabold">Lease </h2>
-            <p>
-              <span className="font-semibold">Start: </span>
-              <span>{start} </span>
-            </p>
-            <p>
-              <span className="font-semibold">End: </span>
-              <span>{end ? end : "Forseeable Future"} </span>
-            </p>
-          </div>
-          <div>
-            <h2 className="mt-2 text-xl font-extrabold">Rent </h2>
-            <p>
-              <span className="font-semibold">Rent: </span>
-              <span>${rent} </span>
-            </p>
-            <p>
-              <span className="font-bold">Deposit: </span>
-              <span>{deposit ? `$${deposit}` : "Nill"} </span>
-            </p>
+              <p>
+                <span className="font-semibold">End: </span>
+                <span>{end ? end : "Forseeable Future"} </span>
+              </p>
+            </div>
+            <div>
+              <h2 className="mt-2 text-xl font-extrabold">Rent </h2>
+              <p>
+                <span className="font-semibold">Rent: </span>
+                <span>${rent} </span>
+              </p>
+              <p>
+                <span className="font-bold">Deposit: </span>
+                <span>{deposit ? `$${deposit}` : "Nill"} </span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
       <div className="flex gap-3">
-        <button className="btn  btn-success text-white rounded-full ">Accept</button>
-        <button className="btn  btn-error text-white rounded-full">Reject</button>
+        <button onClick={acceptHandler} className="btn btn-success text-white rounded-full ">
+          Accept
+        </button>
+        <button onClick={rejectHandler} className="btn btn-error text-white rounded-full">
+          Reject
+        </button>
       </div>
     </OutletPageWrapper>
   )
